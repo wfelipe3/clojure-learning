42f08fd919b3076d76a079e1bda941e2cf55d509
WilliamE <williame@WilliamEs-MacBook-Pro.local>
2017-08-22 15:36:27 -0500

    add function with multiple arguments

--- clojure-basics/test/clojure_basics/flow_control.clj
+++ clojure-basics/test/clojure_basics/flow_control.clj
@@ -15,7 +15,16 @@
                          "empty"
                          "not empty"))))
   (testing "do statement"
-    (is (= :ok (if-not (empty? x) 
+    (is (= :ok (if-not (empty? x)
                  (do (println "ok")
                    :ok)
                  nil)))))
+
+(defn add
+  ([x y] (+ x y))
+  ([x] (add x 1)))
+
+(deftest test-other-thing
+  (testing "something"
+    (is (= 5 (add 2 3)))
+    (is (= 3 (add 2)))))
