e149ab779b8be261ca86b166f084427ed20b2a81
WilliamE <williame@WilliamEs-MacBook-Pro.local>
2017-08-22 08:14:12 -0500

    initial clojure learning project

---
+++ clojure-basics/test/clojure_basics/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-basics.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-basics.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
