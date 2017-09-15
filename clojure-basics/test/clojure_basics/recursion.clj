b6a64062a58acbcd11b18c022612691528549069
WilliamE <williame@WilliamEs-MacBook-Pro.local>
2017-09-15 17:33:05 -0500

    recursive functions

--- clojure-basics/test/clojure_basics/recursion.clj
+++ clojure-basics/test/clojure_basics/recursion.clj
@@ -28,6 +28,18 @@
 (defn reduce-sum-reduced [vals]
   (reduce + vals))
 
+(defn filter-even [acc next-val]
+  (if (even? next-val)
+    (conj acc next-val)
+    acc))
+
+(defn map-inc [acc next-val]
+  (conj acc (inc next-val)))
+
+(defn group-even [acc next-val]
+  (let [key (if (even? next-val) :even :odd)]
+    (update-in acc [key] #(conj % next-val))))
+
 (deftest test-recursion
   (testing "recursive sum of values"
     (is (= 6 (sum-values [1 2 3]))))
@@ -38,4 +50,12 @@
   (testing "reduce sum of values"
     (is (= 6 (reduce-sum [1 2 3]))))
   (testing "reduce sum reduced of values"
-    (is (= 6 (reduce-sum-reduced [1 2 3])))))
+    (is (= 6 (reduce-sum-reduced [1 2 3]))))
+  (testing "reduce to filter values"
+    (is (= [2 4 6] (reduce filter-even [] [1 2 3 4 5 6]))))
+  (testing "filter values"
+    (is (= [2 4 6] (filter even? [1 2 3 4 5 6]))))
+  (testing "map with reduce"
+    (is (= [2 3 4] (reduce map-inc [] [1 2 3]))))
+  (testing "map values"
+    (is (= [2 3 4] (map inc [1 2 3])))))
